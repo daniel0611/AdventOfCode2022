32b263bba1dcb32bdea5b86719b9a98f7757b34f
Daniel Huber <30466471+daniel0611@users.noreply.github.com>
2022-11-07 11:45:07 +0100

    Initial commit

---
+++ add-day/src/main.rs
@@ -0,0 +1,108 @@
+use std::fs;
+use std::io::{self, Write};
+use std::path::Path;
+use std::path::PathBuf;
+
+// This is a utility that asks the user for a input number
+// and then creates a new crate for the advent of code challange of that day.
+
+fn main() {
+    let day = get_day();
+    let day_dir = create_day_dir(day);
+    create_cargo_toml(day, &day_dir);
+    create_src(day, &day_dir);
+    create_input_file(day, &day_dir);
+}
+
+fn get_day() -> u8 {
+    print!("Please enter the day you want to create a crate for: ");
+    io::stdout().flush().unwrap();
+
+    let mut day = String::new();
+    io::stdin()
+        .read_line(&mut day)
+        .expect("Failed to read line");
+    day.trim().parse::<u8>().expect("Please enter a number!")
+}
+
+fn create_day_dir(day: u8) -> PathBuf {
+    let day_directory_str = format!("day{:0>2}", day);
+    let day_directory = PathBuf::from(&day_directory_str);
+    fs::create_dir(&day_directory).expect("Could not create day directory");
+
+    day_directory
+}
+
+fn create_cargo_toml(day: u8, day_dir: &Path) {
+    let cargo_toml_path = day_dir.join("Cargo.toml");
+    let cargo_toml_str = format!(
+        r#"[package]
+name = "day{:02}"
+version = "0.1.0"
+edition = "2021"
+
+[dependencies]
+aoc-utils = {{ path = "../aoc-utils" }}
+"#,
+        day
+    );
+    fs::write(&cargo_toml_path, cargo_toml_str).expect("Could not write Cargo.toml");
+}
+
+fn create_src(day: u8, day_dir: &Path) {
+    let src_dir = &day_dir.join("src");
+    fs::create_dir(src_dir).expect("Could not create src directory");
+
+    let main_rs_path = src_dir.join("main.rs");
+    let main_rs_str = format!(
+        r#"use aoc_utils::PuzzleInput;
+const DAY: u8 = {day};
+
+fn main() {{
+    let input = PuzzleInput::get_input(DAY);
+    println!("A: {{}}", solve_a(&input));
+    println!("B: {{}}", solve_b(&input));
+}}
+
+fn solve_a(input: &PuzzleInput) -> usize {{
+    input.lines().len()
+}}
+
+fn solve_b(input: &PuzzleInput) -> usize {{
+    input.lines().len()
+}}
+
+#[cfg(test)]
+mod tests {{
+    use super::*;
+
+    const TEST_INPUT: &str = "";
+
+    #[test]
+    fn test_no_panic() {{
+        let input = PuzzleInput::get_input(DAY);
+        solve_a(&input);
+        solve_b(&input);
+    }}
+
+    #[test]
+    fn test_solve_a() {{
+        assert_eq!(solve_a(&PuzzleInput::new(TEST_INPUT)), 0);
+    }}
+
+    #[test]
+    fn test_solve_b() {{
+        assert_eq!(solve_b(&PuzzleInput::new(TEST_INPUT)), 0);
+    }}
+}}
+"#,
+        day = day
+    );
+    fs::write(&main_rs_path, main_rs_str).expect("Could not write main.rs");
+}
+
+fn create_input_file(day: u8, day_dir: &Path) {
+    let name = format!("input_day{:02}.txt", day);
+    let input_file_path = day_dir.join(name);
+    fs::File::create(input_file_path).expect("Could not create input file");
+}
